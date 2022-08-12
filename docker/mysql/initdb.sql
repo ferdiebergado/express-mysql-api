75e3a88c8ed9a1014a130ca9e3c64396f4068af0
ferdie <ferdiebergado@gmail.com>
2022-08-12 21:42:24 +0800

    Added mysql entrypoint volume

---
+++ docker/mysql/initdb.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) NOT NULL auto_increment,
+  `email` VARCHAR(150) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE `idx_email_unique` (`email`(255))
+) engine = innodb charset = utf8mb4 COLLATE utf8mb4_general_ci;
