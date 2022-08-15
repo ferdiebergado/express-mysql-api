b38a67ab84ccc3bf0e67f2eee4329b87eefdfd67
ferdie <ferdiebergado@gmail.com>
2022-08-15 19:19:32 +0800

    Fixed imports

--- src/lib/auth/auth.middleware.ts
+++ src/lib/auth/auth.middleware.ts
@@ -1,22 +1,22 @@
 import { Request, Response, NextFunction } from 'express'
-import { UnauthorizedHttpError } from '../http/errors'
-import { verifyToken } from '../utils/jwt'
+import { httpErrors } from '../http'
+import { verifyToken } from '../utils'
 
 // TODO: Add access types
 export const authorize = async (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction
 ) => {
   try {
     const authHeader = req.headers.authorization
     const token = authHeader && authHeader.split(' ')[1]
 
-    if (!token) throw new UnauthorizedHttpError()
+    if (!token) throw new httpErrors.UnauthorizedHttpError()
 
     const user = await verifyToken(token)
 
-    res.locals.user = user
+    req.user = user.id
 
     next()
   } catch (error) {
