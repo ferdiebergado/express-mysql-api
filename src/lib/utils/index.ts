fbb3aef0a9a2aee5b407cb87afa23261bc583499
ferdie <ferdiebergado@gmail.com>
2022-08-15 19:32:04 +0800

    Revert "Removed default user"
    This reverts commit 2f288ca81f94257de7f95931c8f5210dc3e2507b.
    Need to close issue #1

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,13 +1,5 @@
 import { generateToken, verifyToken, JwtPayload } from './jwt'
 import { generateSecureKey } from './crypto'
 import logger from './logger'
-import { validateEnv } from './env-validator'
 
-export {
-  generateToken,
-  verifyToken,
-  generateSecureKey,
-  JwtPayload,
-  logger,
-  validateEnv,
-}
+export { generateToken, verifyToken, generateSecureKey, JwtPayload, logger }
--- src/server.ts
+++ src/server.ts
@@ -2,23 +2,17 @@ import http from 'http'
 import app from './app'
 import { db } from './lib/db'
 
-const HOST = app.get('host')
 const PORT = app.get('port')
-
 const server = http.createServer(app)
 const pool = db.Pool
 
-server.listen(PORT, HOST, () =>
-  console.log('Server listening on http://%s:%d...', HOST, PORT)
-)
+server.listen(PORT, () => console.log('Server listening on port %d...', PORT))
 
 const cleanuUp = async () => {
   // clean up allocated resources
   await pool.release()
-
-  server.close(() => {
-    console.log('Server closed.')
-  })
+  server.close()
+  console.log('Server closed.')
 }
 
 const shutDown = () => {
@@ -27,8 +21,7 @@ const shutDown = () => {
 
 process.on('uncaughtException', async (err) => {
   // log necessary error details to log files
-  console.error('An uncaught error occurred!')
-  console.error(err.stack)
+  console.error(err)
   await cleanuUp()
   shutDown()
 })
